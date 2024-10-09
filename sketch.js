c84be027e0f22a4696e489e1c80e1c0e0f2261c9
Robson125 <robson.malanczyn.lara@escola.pr.gov.br>
2024-10-09 15:05:02 -0300

    Add files via upload
    buscador de filmes

---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+let campoFantlet
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "Fuja";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha";          
+        } else{
+         return "Vovó- zona";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Madagasar";
+        } else {
+          return "Divertidamente";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "Fuja";
+    } else {
+      return "O massacre";
+    }
+  }
+}
